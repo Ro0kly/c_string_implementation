3597d4bf8d13152a0e85d446f5c0b9322a3fceb1
Ro0kly <snallytango@yandex.ru>
2024-07-10 21:01:13 +0300

    main / add strcspn method

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define s21_NULL ((void *)0)
+
 typedef long unsigned s21_size_t;
 
 void *s21_memchr(const void *str, int c, s21_size_t n);
@@ -8,3 +10,4 @@ char *s21_strrchr(const char *str, int c);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
+s21_size_t s21_strcspn(const char *str1, const char *str2);
