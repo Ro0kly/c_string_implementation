2aea06bb1a5ddc01f81af6ea4b35706a6d3f17de
Ro0kly <snallytango@yandex.ru>
2024-07-11 18:59:24 +0300

    main / add strerror method

--- src/s21_string.h
+++ src/s21_string.h
@@ -11,3 +11,4 @@ int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
 s21_size_t s21_strcspn(const char *str1, const char *str2);
+char *s21_strerror(int errorNumber);
