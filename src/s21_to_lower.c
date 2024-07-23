be694ff963eb59671077dc57004ebb31f9b53062
Ro0kly <snallytango@yandex.ru>
2024-07-23 14:47:59 +0300

    main / add to_lower

---
+++ src/s21_to_lower.c
@@ -0,0 +1,18 @@
+#include "s21_string.h"
+
+void *s21_to_lower(const char *str) {
+  char *result_str = s21_NULL;
+  char *buff = (char *)str;
+  size_t len = s21_strlen(buff);
+  result_str = (char *)calloc(len + 1, sizeof(char));
+  if (result_str != s21_NULL) {
+    for (s21_size_t i = 0; i < len; i++) {
+      if (str[i] >= 'A' && str[i] <= 'Z') {
+        result_str[i] = str[i] + 32;
+      } else {
+        result_str[i] = str[i];
+      }
+    }
+  }
+  return result_str;
+}
